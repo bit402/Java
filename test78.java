e75d96b11681e2d8717f15d09f22322ed25ce595
bit402 <1160092090@qq.com>
2026-04-11 09:37:24 +0800

    练习

---
+++ test78.java
@@ -0,0 +1,31 @@
+package test;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class test01 {
+
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        BigInteger a = sc.nextBigInteger();
+//        BigInteger b = sc.nextBigInteger();
+//        BigInteger ans;
+//        ans=a.multiply(b);
+//        System.out.println(ans);
+//        Scanner sc = new Scanner(System.in);
+//        BigInteger a = sc.nextBigInteger();
+//        BigInteger b = sc.nextBigInteger();
+//        BigInteger ans;
+//        ans=a.subtract(b);
+//        System.out.println(ans);
+//        sc.close();
+//        Scanner sc = new Scanner(System.in);
+//        BigInteger a = sc.nextBigInteger();
+//        BigInteger b = sc.nextBigInteger();
+//        BigInteger ans;
+//        ans = a.add(b);
+//        System.out.println(ans);
+//        sc.close();
+        
+    }
+}
